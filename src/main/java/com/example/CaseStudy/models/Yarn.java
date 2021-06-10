9fc21776a27c000811ac1bf25fa23c7be407fdd5
Louisa Molin <louisamolin@gmail.com>
2021-06-09 23:33:13 -0400

    initial commit with some html views

---
+++ src/main/java/com/example/CaseStudy/models/Yarn.java
@@ -0,0 +1,17 @@
+package com.example.CaseStudy.models;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults (level = AccessLevel.PRIVATE)
+public class Yarn {
+    String name;
+    String color;
+    int weight;
+}
