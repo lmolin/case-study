0c781aab50cbf794d33a1da7817f0bbd29a97a2e
Louisa Molin <louisamolin@gmail.com>
2021-06-22 14:52:32 -0400

    users can now create accounts for real this time

--- src/main/java/com/example/CaseStudy/controllers/HomeController.java
+++ src/main/java/com/example/CaseStudy/controllers/HomeController.java
@@ -1,15 +1,57 @@
 package com.example.CaseStudy.controllers;
 
+import com.example.CaseStudy.models.Project;
+import com.example.CaseStudy.models.User;
+import com.example.CaseStudy.services.UserService;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.naming.Binding;
+
+@Log
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserService us;
+
+    @ModelAttribute("user")
+    public User initUser(){
+        return new User();
+    }
 
     //go to homepage
     @GetMapping("/home")
     public String home() {
-        return "projects";
+        return "home";
+    }
+
+    @GetMapping("/settings")
+    public String settings() {
+        return "settings";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @PostMapping("/signup/new")
+    public String newUser(@ModelAttribute("user") @Validated User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            log.warning("invalid credentials");
+            return "signup";
+        }
+
+        us.saveUser(user);
+        return "home";
     }
 
 }
